3940a7f8d3ccfcff4a6e845ded2db0b395e6f661
Eduardo668 <eduardosehn20@gmail.com>
2023-02-27 11:49:03 -0300

    Deploy

--- client/v0.4/src/api/Api.js
+++ client/v0.4/src/api/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const Api = axios.create({
-    baseURL: "https://portifolio-senai-api.fly.dev/api"
+    baseURL: "https://portifoliosenai-api.onrender.com/api"
 })
